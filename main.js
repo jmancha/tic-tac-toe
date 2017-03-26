7c83f8703e2b4b188ef6151b7c59c17fc7daeb7b
jmancha <jessy.mancha@gmail.com>
2017-03-26 17:40:46 -0600

    added more code

--- main.js
+++ main.js
@@ -1,27 +1,64 @@
+$(document).ready(function() {
 
-	
-		function() {}function startGame(){
-			document.turn = "X";
+	var player = 1;
 
-			setmessage(document.turn + "starts first");
-		}
+	$('.square').on('click', function(event) {
 
-		function setmessage(message) {
-			document.getElementById('message').innerText =message;
-		}
-		function nextMove(square){
-			square.innerText = document.turn; 
-			switchTurn();
-		}
+		alert('add symbol here');
+		var squareSelected = $(this);
 
-		function switchTurn() {
-			if (document.turn == "X") {
-				document.turn = "O";
-			} 	
-			else {
-				document.turn == "X";
-
+	 if(squareSelected.hasClass('ex') || squareSelected.hasClass('oh')) {
+			alert('This sqaure has already been selected!');
+		
+		}	else {
+			if(player === 1) {
+				squareSelected.addClass('ex');
+				if(checkIfPlayerwWon('ex');	{
+					alert('Congrats! Player' + player + ' has won')};
+				
+			}	else {
+				player = 2;
 			}
-		}
+						
+			}	else {
+				squareSelected.addClass('oh');
+				if(checkIfPlayerwWon('oh')) {
+					alert('Congrats! Player' + player + ' has won')};
+				}
+			}	else {
+				player = 1;
+			}
+			
+			}
 
+	});
 	
+	function checkIfPlayerwWon(symbol)	{
+
+	if()$('.sq1').hasClass(symbol) && $('.sq2').hasClass(symbol) && $('.sq3').hasClass(symbol)) { 
+		return true;
+			
+	} else if($('.sq4').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq6').hasClass(symbol)) {
+		return true;
+
+	} else if($(.sq7).hasClass(symbol) && $('.sq8').hasClass(symbol) && $('.sq9').hasClass(symbol)) {
+		return true;
+			
+	} else if($('.sq1').hasClass(symbol) && $('sq4').hasClass(symbol) && $('.sq7').hasClass(symbol)) }
+		return true;
+			
+	}else if($('.sq2').hasClass(symbol) && $('.sq4').hasClass(symbol).hasClass(symbol) && $('.sq7').hasClass(symbol){
+		return true;
+			
+	}else if($('.sq3').hasClass(symbol) && $('.sq6').hasClass(symbol).hasClass(symbol) && $('.sq8').hasClass(symbol){
+		return true;
+			
+	} else if($(.sq1).hasClass(symbol) && $('.sq5').hasClass(symbol).hasClass(symbol) && $('.sq9').hasClass(symbol)){
+		return true
+			
+	}else if($('.sq3').hasClass(symbol) && $('sq5').hasClass(symbol).hasClass(symbol) && $('sq7').hasClass(symbol)) }
+		return false;						
+			
+
+	});
+
