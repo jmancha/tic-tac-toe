04398801d7bdbfe454af6e0768aee7eb76198ab8
jmancha <jessy.mancha@gmail.com>
2017-03-30 14:06:06 -0600

    added code again

--- main.js
+++ main.js
@@ -1,35 +1,25 @@
-  function Game() {
-	  this.OMoves = [];
-	  this.XMoves = [];
-	  this.currentTurn = 'X';
+function Game() {
+  this.OMoves = [];
+  this.XMoves = [];
+  this.currentTurn = 'X';
 } 
 
-	Game.winningCombinations = [[0,1,2],[3,4,5],[6,7,8],
-		[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+Game.winningCombinations = [[0,1,2],[3,4,5],[6,7,8],
+[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
-	Game.prototype = {
-
-  		addMove: function(player, box) {
-  			if(player ==='X') {
-  				this.XMoves.push(box);
-  			 	addMove: function (player,box)){;
-    	console.log(this.currentTurn);
-
-    	if(this.currentTurn === 'X') {
-		      this.XMoves.push(box);
-		      this.currentTurn = 'O'; 
-		    } else {
-		      this.OMoves.push(box);
-		      this.currentTurn = 'X';
-
-		    } else 
-		    	this.OMoves.push(box);
-		    	this.OMoves.push(box);
-		    	this.currentTurn = 'X';
-		    }
-	
-  	checkForWin:function(movesArray) {
-    	var result = false;
+Game.prototype = {
+  addMove: function(box) {
+    console.log(this.currentTurn);
+    if(this.currentTurn === 'X') {
+      this.XMoves.push(box);
+      this.currentTurn = 'O'; 
+    } else {
+      this.OMoves.push(box);
+      this.currentTurn = 'X';
+    }
+  }, 
+  checkForWin: function(movesArray){
+    var result = false;
 
     function comboChecker(previousValue, checkValue){
       if (movesArray.indexOf(checkValue) !== -1){
@@ -49,17 +39,16 @@
     Game.winningCombinations.forEach(checkWinning);
 
     return result;
-  }
-  
+  },
   isDraw: function(){
     return this.XMoves.length + this.OMoves.length === 9;
-  }
-   reset: function() {
+  },
+  reset: function() {
     this.XMoves = [];
     this.OMoves = [];
     this.currentTurn = 'X';
   }
-}
+};
 
 function CellView(number){
   this.id = number;
@@ -80,108 +69,71 @@ CellView.prototype = {
   }
 };
 
-	window.onload = start;
-		var boxes = document.getElementsByTagName("td");
-		var turnText = document.querySelector(".playerTurn");
-		var counter = 1;
-		var winCounter = 0;
-		var OMoves = [];
-		var XMoves = [];
-		var winningCombinations = [[0,1,2],[3,4,5],[6,7,8],
-								-[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+window.onload = start;
+var boxes = document.getElementsByTagName("td");
+var turnText = document.querySelector(".playerTurn");
+var winCounter = 0;
 
-		var theGame = new Game();
-		var board = [];
+var theGame = new Game();
+var board = [];
 
- function start(){
-		  	theGame.reset();
-		  for(var i=0; i < 9; i++){
-		    board.push(new CellView(i));
-		  }
-		  addXandOListener();
-		  addResetListener();
-		}
+function start(){
+  theGame.reset();
+  for(var i=0; i < 9; i++){
+    board.push(new CellView(i));
+  }
+  addXandOListener();
+  addResetListener();
+}
 
- function addXandO(event){
- 	if (event.target.innerHTML.length === 0) {
- 		var boxNum = parseInt(event.target.getAttribute("data-num"));
- 		
- 		if (counter % 2 === 0) {
- 			OMoves.push(boxNum);
- 			event.target.setAttribute("class","O");  
-		}
-		if (theGame.currentTurn === 'O'){
-			turnText.innerHTML = "It is X's turn";
-			counter++;
-			theGame.addMove("O", boxNum);
-			board[boxNum].addMark('O');
-			checkForWin(theGame.OMoves, "O");
-
-		}
-		else {
-			XMoves.push(boxNum);
-			event.target.innerHTML = "X";
-			event.target.setAttribute("class","X");
-			turnText.innerHTML = "it's is "O" turn";
-			counter++;
-
-			theGame.addMove("X", boxNum);
-			board[boxNum].addMark('X');
-			checkForWin(theGame,XMoves, "X");
-
-		}
-
-		if (theGame.isDraw()) {
-
-		}
+function addXandOListener(){
+  for (var i = boxes.length - 1; i >= 0; i--) {
+    boxes[i].addEventListener("click", addXorO);
+  }
+}
 
 function addXorO(event){
-  if (event.target.innerHTML.length === 0)
+  if (event.target.innerHTML.length === 0){
     var boxNum = parseInt(event.target.getAttribute("data-num"));
-
-
+    console.log(theGame.currentTurn);
     if (theGame.currentTurn === 'O'){
       turnText.innerHTML = "It is X's turn";
       board[boxNum].addMark('O');
       checkForWin(theGame.OMoves, "O");
     }
-}
-    else {} {
-
+    else {
       turnText.innerHTML = "It is O's turn";
       board[boxNum].addMark('X');
       checkForWin(theGame.XMoves, "X");
     }
-
   if (theGame.isDraw()){
     turnText.innerHTML = "Game Over!";
     var conf = confirm("It's a draw, do you want to play again?");
-  if(conf){
+    if(conf){
       resetBoard();
-		}
-	}
-
- function checkForWin(movesArray, name) {
-  if (theGame.checkForWin(movesArray)){
-    	alert("Game over, " + name + " wins!");
-    	resetBoard();
-
- 	 		boxes[i].innerHTML="";
- 	 		boxes[i].setAttribute("class","clear"); 		
+    }
   }
+ }
+}
 
-	}
+function addResetListener(){
+  var resetButton = document.getElementById("reset");
+  resetButton.addEventListener("click", resetBoard);
+}
 
-		OMoves = [];
+function checkForWin(movesArray, name){
+  if (theGame.checkForWin(movesArray)){
+    alert("Game over, " + name + " wins!");
+    resetBoard();
+  }
+}
 
-		XMoves = [];
-
-		winCounter=0;
-
-		counter = 1;
-
-		turnText.innerHTML = "it is X's turn";
-
-		theGame.reset();
-
+function resetBoard(){
+  for (var i = boxes.length - 1; i >= 0; i--) {
+    boxes[i].innerHTML="";
+    boxes[i].setAttribute("class","clear");
+  }
+  winCounter=0;
+  turnText.innerHTML = "It is X's turn";
+  theGame.reset();
 }
